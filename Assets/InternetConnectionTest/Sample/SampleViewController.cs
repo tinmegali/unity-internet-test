b066d2b38faab07c2fcb055f8cd7e688be81bddf
Tin Megali <tinmegali@gmail.com>
2019-10-09 14:52:46 -0400

    first commit

---
+++ Assets/InternetConnectionTest/Sample/SampleViewController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleViewController : MonoBehaviour
+{
+    public Text FeedbackText;
+    public Button ButtonTestInternet;
+
+    private ConnectionTester _connectionTester;
+    void Start()
+    {
+        _connectionTester = ConnectionTester
+            .GetInstance(gameObject)
+            .ipToTest("www.google.com");
+        
+        ButtonTestInternet.onClick.AddListener(() =>
+        {
+            ShowFeedback("Starting test");
+            _connectionTester.TestInternet(hasInternet =>
+            {
+                ShowFeedback($"Has internet connection: {hasInternet}");
+            });
+        });
+
+    }
+
+    void ShowFeedback(string text)
+    {
+        FeedbackText.text = text;
+    }
+}
